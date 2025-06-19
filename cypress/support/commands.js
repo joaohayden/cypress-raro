ca8db1fca13232e96f454e115a9b283aa1fe22f6
joaohayden <joao.hayden@ubistart.com>
2025-06-19 00:33:30 -0400

    add loginUI command on project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const {users, password} = Cypress.env()
+
+//login via UI
+Cypress.Commands.add('loginSauce',(userType) => {
+    cy.visit('https://saucedemo.com/')
+    cy.get('[data-test="username"]').type(users[userType])
+    cy.get('[data-test="password"]').type(password)
+    cy.get('[data-test="login-button"]').click()
+})
